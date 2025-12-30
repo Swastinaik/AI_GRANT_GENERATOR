db8e58260dac88f85ba2480050c1db877d0d754f
swasti <swastinaik273@gmail.com>
2025-12-30 05:35:09 +0000

    add the metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { Roboto } from "next/font/google"
+import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider"
 import { Toaster } from "@/components/ui/sonner"
@@ -7,12 +8,45 @@ import NavBar from "@/components/NavBar";
 import { usePathname } from "next/navigation";
 import path from "path";
 
- const roboto = Roboto({
+const roboto = Roboto({
   weight: ["400", "500", "700"], // You can add '100', '300', '900' etc.
   subsets: ["latin"],
   display: "swap", // Ensures text is visible while font loads
   variable: "--font-roboto", // Optional: create a CSS variable
 });
+
+export const metadata: Metadata = {
+
+  title: "Docs4All",
+  description:
+    "An Unified Platform for AI Agents to get things done",
+
+  openGraph: {
+    title: "Docs4All",
+    description:
+      "An Unified Platform for AI Agents to get things done",
+    url: "https://ai-grant-generator.vercel.app/",
+    siteName: "Docs4All",
+    images: [
+      {
+        url: "https://ai-grant-generator.vercel.app/docs4all.png",
+        width: 1894,
+        height: 990,
+        alt: "Docs4All",
+      },
+    ],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary",
+    title: "Docs4All",
+    description:
+      "An Unified Platform for AI Agents to get things done",
+    images: ["https://ai-grant-generator.vercel.app/docs4all.png"],
+  },
+
+};
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -23,8 +57,8 @@ export default function RootLayout({
   const loginPage = pathname.startsWith('/login')
   const registerPage = pathname.startsWith('/register')
 
-  
- 
+
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body
@@ -32,15 +66,15 @@ export default function RootLayout({
         className={roboto.className}
       >
         <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {!landingpage && !loginPage && !registerPage && <NavBar />}
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {!landingpage && !loginPage && !registerPage && <NavBar />}
           {children}
           <Toaster />
-          </ThemeProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
